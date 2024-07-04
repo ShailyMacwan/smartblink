36866dbbbbd92ea60ba1aac4ef2c4c9269a698d7
Shaily Macwan <shailymacwan12@gmail.com>
2024-07-04 02:29:15 -0400

    Home_Page_01

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import hmBackground from '../assets/home-bg.jpg'
-import home01 from "../assets/home-01.jpg"
+import React from 'react';
+import { Button, Container, Typography, Box } from '@mui/material';
+import hmBackground from '../assets/home-bg.jpg';
+import home01 from '../assets/home-01.jpg';
+import home1 from '../assets/home-1.jpg';
+import home2 from '../assets/home-2.jpeg';
+import home3 from '../assets/home-3.png';
+import icone1 from '../assets/icon1.png';
+import icone2 from '../assets/icon2.png';
+import icone3 from '../assets/icon3.png';
+import icone4 from '../assets/icon4.png';
+import icone5 from '../assets/icon5.png';
+import icone6 from '../assets/icon6.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 export default function Home() {
 
   return (
-    <>
+  <>
 
     <div className="image-container">
       <img src={home01} alt="Descriptive Text" className="image" />
@@ -32,6 +44,82 @@ export default function Home() {
           </h5>
       </div>
     </div> 
+
+    <div className="text-center">
+  <h2 style={{ color: '#14274C' }}>FEATURES</h2>
+
+  <div className="features">
+    <div>
+      <div className='features_containers'>
+        <img src={icone1} alt='icone1' />
+      </div>
+      <div className='features_text'>
+        Advanced Home Automation Solutions
+      </div>
+    </div>
+
+    <div>
+      <div className='features_containers'>
+        <img src={icone2} alt='icone2' />
+      </div>
+      <div className='features_text'>
+        Smart Lighting
+      </div>
+    </div>
+
+    <div>
+      <div className='features_containers'>
+        <img src={icone3} alt='icone3' />
+      </div>
+      <div className='features_text'>
+        Security System
+      </div>
+    </div>
+
+    <div>
+      <div className='features_containers'>
+        <img src={icone4} alt='icone4' />
+      </div>
+      <div className='features_text'>
+        Entertainment
+      </div>
+    </div>
+
+    <div>
+      <div className='features_containers'>
+        <img src={icone5} alt='icone5' />
+      </div>
+      <div className='features_text'>
+        Climate Control
+      </div>
+    </div>
+
+    <div>
+      <div className='features_containers'>
+        <img src={icone6} alt='icone6' />
+      </div>
+      <div className='features_text'>
+        User-friendly App
+      </div>
+    </div>
+  </div>
+</div>
+
+
+    <div className="homeImage">
+        <div className="row">
+            <div className="column">
+                <img src={home1} alt="Snow" />
+            </div>
+            <div className="column">
+                <img src={home2} alt="Forest" />
+            </div>
+            <div className="column">
+                <img src={home3} alt="Mountains" />
+            </div>
+        </div>
+    </div>
+   
       
   </>
-)};
+)}
